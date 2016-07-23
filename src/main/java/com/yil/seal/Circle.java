751e856954f10ec8e7c54c0c07b71345f5e203eb
yilei158 <yilei158@126.com>
2016-07-23 13:37:27 +0800

    init project

---
+++ src/main/java/com/yil/seal/Circle.java
@@ -0,0 +1,10 @@
+package com.yil.seal;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("draw a circle png ");
+	}
+
+}
